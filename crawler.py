c1742e77a834fa712f2eff7313dffdf88ac363a0
TheFabulousP <anphu.hlap@gmail.com>
2024-05-31 20:41:56 +0700

    update url

--- crawler.py
+++ crawler.py
@@ -1,53 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
 from database import LINKS_COLLECTION, SOURCES_COLLECTION
+from urls import news_urls
 from redis_cache import REDIS_CACHE
-
-news_urls = [
-  ('https://vnexpress.net/kinh-doanh/chung-khoan', 'https://vnexpress.net', 'title-news', 'description'),
-  ('https://kinhtedothi.vn/tai-chinh-chung-khoan.html', 'https://kinhtedothi.vn', 'story__title', 'story__summary'),
-]
-
+  
 def extract_link(title_element, site):
-  link = title_element.find('a').get('href')
-  if link[0] == "/":
+  link = title_element.find('a')
+  if link is None:
+    link = title_element
+  link = link.get("href")
+  if link[0] == "/" or link[0] == ".":
     link = site + link
   return link
 
-def fetch_news(url, site, titleTag, summaryTag):
+def fetch_news(source, site, urlTag, titleTag, summaryTag):
   try:
-    response = requests.get(url)
+    response = requests.get(source)
     response.raise_for_status()
     page_content = response.content
     soup = BeautifulSoup(page_content, 'html.parser')
 
     # Extract the news content based on HTML structure
     title_elements = soup.find_all(class_=titleTag)
-    links = [extract_link(title_element, site) for title_element in title_elements]
+    url_elements = soup.find_all(class_=urlTag)
+    print(title_elements)
+    # print(len(title_elements))
+    for title_element in title_elements:
+      print(title_element)
+    links = [extract_link(url_element, site) for url_element in url_elements]
     titles = [title_element.get_text().strip() for title_element in title_elements]
+    print(links)
+    print(titles)
     description_elements = soup.find_all(class_=summaryTag)
     descriptions = [description.get_text().strip() for description in description_elements]
+    
+    # If there is no description, fi
+    if descriptions == []:
+      descriptions = ["" for i in titles]
 
     articles = [{
       'title': title,
+      'source': source,
       'site': site,
       'link': link,
       'summary': description
     } for title, link, description in zip(titles, links, descriptions)]
     print(articles)
 
+    LINKS_COLLECTION.delete_many({"source": source})
     for article in articles:
       LINKS_COLLECTION.insert_one(article)
       
-    return {'url': url, 'status': 'success'}
+    return {'url': source, 'status': 'success'}
   except requests.RequestException as e:
-      return {'url': url, 'error': str(e)}
+      return {'url': source, 'error': str(e)}
   except Exception as e:
-      return {'url': url, 'error': str(e)}
+      return {'url': source, 'error': str(e)}
   
-for element in news_urls:
-  url, site, titleTag, summaryTag = element
-  fetch_news(url, site, titleTag, summaryTag)
+for i in range(0, 42):
+  try:
+    document = SOURCES_COLLECTION.find_one({"id": i})
+    source = document["source"]
+    site = document["site"]
+    urlTag = document["urlTag"]
+    titleTag = document["titleTag"]
+    summaryTag = document["summaryTag"]
+    print(fetch_news(source, site, urlTag, titleTag, summaryTag))
+  except:
+    print("Error occurs when crawling from " + SOURCES_COLLECTION.find_one({"id": i})["source"])
 
-REDIS_CACHE.set("current_index", 1)
-print(int(REDIS_CACHE.get("current_index")))
--- redis_cache.py
+++ redis_cache.py
@@ -6,3 +6,6 @@ REDIS_CACHE = redis.Redis(
   port=REDIS_PORT,
   password=REDIS_PASSWORD
 )
+
+REDIS_CACHE.set("current_index", 1)
+print(int(REDIS_CACHE.get("current_index")))
